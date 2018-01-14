9c21951322207e76bbdb24f0535dc4a1759e6f6e
Kayvan Mazaheri <KayvanMazaheri@GMail.com>
2018-01-15 00:45:40 +0330

    Add SocketIO Support to Client

--- client/src/main.js
+++ client/src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 
+import VueSocketio from 'vue-socket.io'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
@@ -11,6 +12,7 @@ import router from './router'
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
+Vue.use(VueSocketio, 'http://localhost:3000')
 
 /* eslint-disable no-new */
 new Vue({
