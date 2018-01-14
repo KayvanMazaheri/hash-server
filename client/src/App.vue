a9bf31f46f6102af171b4a7de00eb59c7e16861f
Kayvan Mazaheri <KayvanMazaheri@GMail.com>
2018-01-15 00:46:35 +0330

    Improve Application Layout

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <el-container>
+      <el-header height="13em">
+        <img src="./assets/logo.png" class="logo">
+      </el-header>
+      <el-main>
+        <router-view/>
+      </el-main>
+      <el-footer>
+        <p>Secure Remote Hashing Server - IT Engineering Course @ IUT</p>
+      </el-footer>
+    </el-container>
   </div>
 </template>
 
@@ -12,12 +21,17 @@ export default {
 </script>
 
 <style>
-#app {
+html {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+#app {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+img.logo {
+  height: 12em
 }
 </style>
