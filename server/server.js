2b7401896dd023d3870a375861ad849772bfad7b
Kayvan Mazaheri <KayvanMazaheri@GMail.com>
2018-01-14 21:01:55 +0330

    Replace mongoose with nedb

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const info = require('./package.json')
 const express = require('express')
-const q = require('q')
-const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const socketio = require('socket.io')
 const http = require('http')
+
+const { socketController } = require('./controllers')
 
 // Load Environment Variables
 dotenv.load()
@@ -14,17 +14,11 @@ const app = express()
 const httpServer = http.Server(app)
 const io = socketio(httpServer)
 
+// Load the socket.io controllers
+io.on('connection', socketController)
+
 // Set port number for app
 app.set('port', process.env.PORT || 3000)
-
-// plug q as mongoose primary promise
-mongoose.Promise = q.Promise
-
-// connect to db, must make sure connection is established
-mongoose.connect(process.env.MONGO_URI, { useMongoClient: true })
-mongoose.connection.on('error', () => {
-  throw new Error(`unable to connect to database: ${process.env.MONGO_URI}`)
-})
 
 // Listen for connections
 httpServer.listen(app.get('port'), function () {
