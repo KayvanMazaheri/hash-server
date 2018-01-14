31d0ddd8ad3c780925042ec49b64c2c419e2922c
Kayvan Mazaheri <KayvanMazaheri@GMail.com>
2018-01-15 00:44:29 +0330

    Add User Model

---
+++ server/models/User.js
@@ -0,0 +1,38 @@
+const bcrypt = require('bcrypt')
+
+const Datastore = require('nedb')
+
+let dbPath = process.env.DB_PATH || 'database.db'
+
+const hashPassword = (user, cb) => {
+  bcrypt.genSalt(10, function (err, salt) {
+    if (err) {
+      cb(err)
+    }
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) {
+        cb(err)
+      }
+      user.password = hash
+      cb(null, user)
+    })
+  })
+}
+
+const comparePassword = (user, password, cb) => {
+  bcrypt.compare(password, user.password, function (err, isMatch) {
+    cb(err, isMatch)
+  })
+}
+
+let Users = new Datastore({ filename: dbPath, autoload: true })
+
+Users.ensureIndex({ fieldName: 'username', unique: true }, function (err) {
+  console.log(`error happened in saving user: ${err}`)
+})
+
+module.exports = {
+  Users,
+  hashPassword,
+  comparePassword
+}
