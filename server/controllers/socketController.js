8d69ca08fa912245cffdcba0fcb1442f3a9b4cec
Kayvan Mazaheri <KayvanMazaheri@GMail.com>
2018-01-15 10:28:19 +0330

    Implement Registration Process

--- server/controllers/socketController.js
+++ server/controllers/socketController.js
@@ -1,6 +1,9 @@
 const AES = require('../helpers/aes')
 const RSA = require('../helpers/rsa')
+const validator = require('../helpers/validator')
 const crypto = require('crypto')
+
+const Users = require('../models/User')
 
 module.exports = function socketController (socket) {
   console.log(`${socket.id} connected`)
@@ -20,7 +23,7 @@ module.exports = function socketController (socket) {
   socket.on('handshake', data => {
     data = JSON.parse(data)
     socket.data.client.publicKey = data.publicKey
-    console.log(`handshake from ${socket.id}: PK is ${socket.data.client.publicKey}`)
+    console.log(`handshake from ${socket.id}. Public Key:\n ${socket.data.client.publicKey}`)
 
     let encryptedAESKey = RSA.encrypt(socket.data.client.publicKey, socket.data.common.aesKey)
     let handshakeData = {
@@ -29,13 +32,60 @@ module.exports = function socketController (socket) {
     }
     let sign = RSA.sign(socket.data.server.privateKey, JSON.stringify(handshakeData))
 
-    let handshakePackage = { handshakeData, sign }
+    let handshakePackage = { data: handshakeData, sign }
     let encryptedHandshakePackage = AES.createAesMessage(socket.data.common.aesKey, JSON.stringify(handshakePackage))
 
     socket.emit('handshake', { encryptedAESKey, encryptedHandshakePackage })
 
     // Now client can either register or authenticate
-    socket.on('register', data => {})
+    socket.on('register', data => {
+      data = JSON.parse(data)
+
+      let decryptedData = JSON.parse(AES.decrypt(socket.data.common.aesKey, data))
+      let request = decryptedData.data
+      let signature = decryptedData.sign
+
+      let integrity = RSA.verify(socket.data.client.publicKey, JSON.stringify(request), signature)
+      console.log(`register request from ${socket.id}. integrity check ${(integrity ? 'pass' : 'fail')}ed`)
+
+      if (!integrity) {
+        socket.emit('error', 'integrity check failed')
+      } else if (!validator.registrationRequest(request)) {
+        console.log(`register request from ${socket.id} failed: invalid request schema`)
+        socket.emit('error', 'invalid request schema')
+      } else {
+        let newUser = {
+          username: request.username,
+          password: request.password,
+          publicKey: socket.data.client.publicKey
+        }
+        Users.hashPassword(newUser, (err, user) => {
+          if (err) {
+            console.log(`register request from ${socket.id} failed: ${err}`)
+            socket.emit('error', 'internal error happened')
+          } else {
+            Users.Users.insert(user, (err, savedUser) => {
+              if (err) {
+                console.log(`register request from ${socket.id} failed: ${err}`)
+                socket.emit('error', `registration error happened`)
+              } else {
+                console.log(`register request from ${socket.id} succeeded: new user ${newUser._id} saved`)
+                // remove the password field from the user object
+                delete savedUser.password
+
+                let sign = RSA.sign(socket.data.server.privateKey, JSON.stringify(savedUser))
+                let response = { data: savedUser, sign }
+
+                let encryptedResponse = AES.createAesMessage(socket.data.common.aesKey, JSON.stringify(response))
+
+                socket.emit('register', { encryptedResponse })
+              }
+            })
+          }
+        })
+      }
+
+    })
     socket.on('auth', data => {})
   })
   socket.on('disconnect', () => {
